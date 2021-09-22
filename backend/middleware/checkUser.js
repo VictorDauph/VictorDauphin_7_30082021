acbfc56a3cda9dd5132bb7829d13de641d7b78db
VictorDauph <victor.dauph@gmail.com>
2021-09-22 10:22:40 +0200

    User routes completed

--- backend/middleware/checkUser.js
+++ backend/middleware/checkUser.js
@@ -6,17 +6,18 @@ const secretKey = process.env.SECRET_KEY;
 
 module.exports = (req, res, next) => {
     try {
-        console.log("demande d'authentification")
+        console.log("Vérification de l'utilisateur")
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, secretKey); //clé de cryptage. elle doit correspondre à la clé utilisée dans la fonction login.
         const userId = decodedToken.userId;
-        console.log("token user ID: ", userId, "request user ID: ", req.body.userId )
-        if (req.body.userId && req.body.userId !== userId){ // S'il y'a un user Id dans la requête et qu'il est différent de celui contenu dans le token
-            throw 'User Id non valable';
+        console.log("token user ID: ", userId, "http request id: ", req.params.id )
+        
+        if (req.params.id != userId){ // Si l'userId passé par la requête http est différent de celui contenu dans le token
+            throw 'Action non autorisée pour cet utilisateur';
         } else {
             next(); /*Ce middleware est utilisé par d'autres middlewares avant de faire quoi que se soit. si l'authentification rate, 
                         alors il bloque le processus. Sinon il passe au middleware suivant.*/
-         }
+         } 
     } catch (error) {
         res.status(401).json({error});
     }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,9 +10,12 @@ const userCtrl = require('../controllers/userControllers');
 //importation du fichier d'authentification par token
 const auth = require("../middleware/auth")
 
+//importation du fichier de vérification des droits utilisateur
+const checkUser = require("../middleware/checkUser")
+
 //on utilise des routes post car les requêtes doivent contenir des informations: email et mdp 
 router.post('/signup',userCtrl.signup);
 router.post('/login',userCtrl.login);
-router.delete('/:id',auth,userCtrl.delete);
+router.delete('/:id',auth,checkUser, userCtrl.delete);
 
 module.exports = router;
