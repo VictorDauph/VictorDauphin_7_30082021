7ed4522d73512e58cc882f38a8cce36dd4ce61e3
VictorDauph <victor.dauph@gmail.com>
2021-09-28 15:41:06 +0200

    gestion erreur a reparer

--- frontend/src/JS/signup/signupPage.js
+++ frontend/src/JS/signup/signupPage.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     state = {
 
         title: "Création du compte Utilisateur",
+        message:"rien"
     
       };
     
@@ -25,7 +26,13 @@ class App extends React.Component {
             headers: requestHeaders,
         };
         fetch("http://localhost:4000/api/account/signup",init
-        ).then(data => console.log(data)).catch(err => console.log(err)) 
+        ).then(res => {
+            console.log("status pre-json",res.status)
+            return res.json()}).then(data =>{
+            this.setState({message:"message: "+data.message}).catch(err => console.log("erreure attrappée"))
+        }) 
+               
+
     }; 
         
 
@@ -37,7 +44,7 @@ class App extends React.Component {
                     <h2 class="text-primary col-10">{this.state.title}</h2>
                 </div>
                 <SignupForm onUserAdd={this.handleAdd} /> {/*  Appelle signupForm et lui indique de s'afficher ici et Rend disponible la fonction handleadd(requête Post à l'API) au sous-fichier user add qui gère le formulaire */}
-                <p class="text-danger">{message}</p>
+                <p class="text-danger">{this.state.message}</p>
             </div>
         );
     }
