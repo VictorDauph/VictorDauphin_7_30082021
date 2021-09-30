c388555a91f765798378695c8b123f3e4fd4d287
VictorDauph <victor.dauph@gmail.com>
2021-09-30 15:14:03 +0200

    routes commentaires

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -39,3 +39,46 @@ exports.createComment = (req, res, next) =>{
     })
     .catch(error => res.status(404).json({"message":error}));
   }
+
+  exports.flag = (req,res,next) =>{
+    console.log("demande de signalement du post :", req.params.commentId)
+    Comment.findOne({ where: {commentId: req.params.commentId }})
+    .then(comment => {
+        comment.flagged = true;
+        comment.save();
+      res.status(201).json({"message":"comment "+ req.params.commentId +" flagged"})
+    })
+    .catch(error => res.status(404).json({"message":error}));
+  }
+  
+  exports.unflag = (req,res,next) =>{
+    console.log("demande de désignalement du post :", req.params.commentId)
+    Comment.findOne({ where: {commentId: req.params.commentId }})
+    .then(comment => {
+        comment.flagged = false;
+        comment.save();
+      res.status(201).json({"message":"comment "+ req.params.commentId +" unflagged"})
+    })
+    .catch(error => res.status(404).json({"message":error}));
+  }
+
+  // Fonction de récupération de tous les commentaires d'un post
+exports.getCommentsFromPost = (req, res, next) =>{
+    Comment.findAll({ where: {postId: req.params.postId }}) //_id est le champs de l'objet thing évalué, req.params.id est la valeur recherchée passée par la requête HTTP (:id). On va donc regarder tous les objets dans la BDD et chercher celui qui a le bon ID.
+    .then(comments => res.status(200).json(comments))
+    .catch(error => res.status(404).json({message:error}));
+  };
+
+    // Fonction de récupération des commentaires signalés
+  exports.getFlaggedComments = (req, res, next) =>{
+    Comment.findAll({ where: {flagged:true }}) //Chercher tous les comments flaggés
+    .then(comments => res.status(200).json(comments))
+    .catch(error => res.status(404).json({message:error}));
+  };
+
+      // Fonction de récupération d'un commentaire pour admin
+      exports.getOneComment = (req, res, next) =>{
+        Comment.findOne({ where: {commentId: req.params.commentId }}) //Chercher un commentaire par son Id
+        .then(comment => res.status(200).json(comment))
+        .catch(error => res.status(404).json({message:error}));
+      };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 //chargement du middleware niveau router
 const router = express.Router();
 
-//importation du middleware d'authentification
+//importation du middleware d'authentification pour les actions réservées aux admins.
 const adminAuth = require('../middleware/adminAuth')
 
 //importation fichier controlleur
@@ -26,5 +26,19 @@ router.delete('/:userId/:commentId',auth,checkUser,checkCommentsRights,commentCt
 //Modification commentaire
 router.put('/:userId/:commentId',auth,checkUser,checkCommentsRights,commentCtrl.modifyComment);
 
+// Cette route permet de signaler un commentaire
+router.put('/flag/:userId/:commentId', auth, commentCtrl.flag);
+
+// Cette route permet de désignaler un commentaire
+router.put('/unflag/:userId/:commentId',auth, adminAuth, commentCtrl.unflag); 
+
+//Cette route permet de récupérer tous les commentaires liés à un post
+router.get("/getFrom/:postId",auth,commentCtrl.getCommentsFromPost)
+
+//Fetch tous les commentaires signalés pour besoins de modération
+router.get("/flagged",auth,adminAuth,commentCtrl.getFlaggedComments)
+
+//Fetch un commentaire pour besoins de modération
+router.get("/flagged/:commentId",auth,adminAuth,commentCtrl.getOneComment)
 
 module.exports = router;
