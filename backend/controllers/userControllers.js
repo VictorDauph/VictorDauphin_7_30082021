067048a04297adafefe79e9f4192ae002ac12661
VictorDauph <victor.dauph@gmail.com>
2021-10-21 15:08:11 +0200

    correction mineur controlleur retrieve user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -94,9 +94,10 @@ exports.delete = (req, res, next) =>{
 
   //fonction de récupération d'un email utilisateur
 exports.retrieveUser = (req, res, next) =>{
+    console.log("retrieving user from id: ", req.params.userToRetrieve )
     User.findOne({where:{userId: req.params.userToRetrieve}})
     .then( user => {
         console.log("user Id:", user.userId, "user email :", user.email )
         res.status(201).json({email:user.email})
-    }).catch(error => res.status(404).json({message:error}));
+    }).catch(error => res.status(404).json({message:"utilisateur introuvable"}));
 }
